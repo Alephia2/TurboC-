0ea8c286a97cf5a4c484859b0515ce3b2bb689ff
Andy <Andyraill14@gmail.com>
2026-09-04 07:57:17 +0800

    update

--- restaurant.cpp
+++ restaurant.cpp
@@ -1,9 +1,10 @@
+
 #include <iostream>
 #include <stdio.h>
 #include <iomanip>
-
+ 
 using namespace std;
-
+ 
 void display_initial2 () {
     cout << "\n   ----------WELCOME----------\n";
     cout << " ________________________________  \n";
@@ -12,12 +13,12 @@ void display_initial2 () {
     cout << "|3         |        30%' discount| \n";
     cout << "|__________|_____________________| \n\n";
 }
-
+ 
 void askForName ( char* name, int size) {
     cout << "Enter Name     :     ";
      cin.getline(name, size); 
 }
-
+ 
 float askForValue (const char* prompt) {
     float value;
     cout << prompt; 
@@ -25,12 +26,12 @@ float askForValue (const char* prompt) {
     cin.ignore(1000, '\n'); 
     return value;
 }
-
+ 
 float getAmount (float price, float quantity){
     float amount = price * quantity;
     return amount;
 }
-
+ 
 float getDiscount (int discount, float amount, float totalAmount) {
     float finalPrice;
     int disvar;
@@ -52,11 +53,19 @@ float getDiscount (int discount, float amount, float totalAmount) {
         }
         default: {
             cout << "Invalid Discount Price!";
+            finalPrice = amount;
+            break;
         }
     }
+ 
+    // Additional 10% discount if the purchase amount exceeds 10,000
+    if (totalAmount > 10000) {
+        finalPrice = finalPrice - (finalPrice * 0.10);
+    }
+ 
 return finalPrice;
 }
-
+ 
 void print (char name[][35], float price[], float quantity[], int discount[], float amount[], float totalAmount, float finalAmount, float discAmount[], float newPrice[]){
     cout << "============================================================================================\n";
     cout << "                                        FINAL RECEIPT                                       \n";
@@ -69,7 +78,7 @@ void print (char name[][35], float price[], float quantity[], int discount[], fl
          << left << setw(17) << "NEW PRICE";
     cout << "\n------------------------------------------------------------------------------------------\n";
     
-    for (int i = 0 ; i < 2 ; i++){    
+    for (int i = 0 ; i < 10 ; i++){    
     int discPercent = (discount[i] == 1) ? 10
                          : (discount[i] == 2) ? 20
                          : (discount[i] == 3) ? 30
@@ -87,22 +96,22 @@ cout << "-----------------------------------------------------------------------
 cout << "                         TOTAL AMOUNT OF PURCHASE:            "<< totalAmount << endl;
 cout << "                         TOTAL ALLOVER DISCOUNT :             "<< (totalAmount - finalAmount) << endl;
 cout << "                         TOTAL NEW PRICE OF PURCHASE:         "<< finalAmount << endl;
-
-
+ 
+ 
 }
-
-
-
-
+ 
+ 
+ 
+ 
 int main(){
-    char name[2][35];
-    int discount[2];
-    float price[2], quantity[2], amount[2], totalAmount, finalPrice, finalAmount, disvar;
-    float discAmount[2], newPrice[2];
+    char name[10][35];
+    int discount[10];
+    float price[10], quantity[10], amount[10], totalAmount, finalAmount;
+    float discAmount[10], newPrice[10];
     totalAmount = 0;
     finalAmount = 0;
-
-for (int i = 0 ; i < 2 ; i++){
+ 
+for (int i = 0 ; i < 10 ; i++){
    askForName(name[i], 35);
    display_initial2();
     price[i] = askForValue("Enter Price        :     ");
@@ -112,20 +121,15 @@ for (int i = 0 ; i < 2 ; i++){
   
     amount[i] =  getAmount(price[i], quantity[i]);
     totalAmount += amount[i];
-
+ 
     newPrice[i] = getDiscount(discount[i], amount[i], totalAmount);
     discAmount[i] = amount[i] - newPrice[i];
     finalAmount += newPrice[i];
-
-    if (totalAmount > 10000) {
-         finalPrice = amount[i]  - (amount[i] * 0.10); 
-         disvar = finalPrice - totalAmount;
-    }
-
+ 
 }
     print (name,  price,  quantity,  discount,  amount,  totalAmount,  finalAmount, discAmount, newPrice);
-
-
-
-
+ 
+ 
+ 
+ 
 }
